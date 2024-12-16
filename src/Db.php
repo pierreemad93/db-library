322f8af3cf2701d96960bca4766825ffdf2c8e22
pierreemad93 <pierreemad93@outlook.com>
2024-12-16 13:47:41 +0200

    first commit

---
+++ src/Db.php
@@ -0,0 +1,35 @@
+<?php
+namespace  Bebos\DbBulider ;
+use PDO;
+
+abstract class Db
+{
+
+    protected $query;
+    protected $connection;
+    public $stmt;
+
+    public function __construct($dsn, $username, $password)
+    {
+        $this->connection = new PDO($dsn, $username, $password);
+    }
+
+    abstract public function index(string $table, array|string $columns = "*");
+
+    abstract public function store(string $table, array $data);
+
+    abstract public function update();
+
+    abstract public function destroy();
+
+    final public function execute()
+    {
+        $this->stmt->execute();
+        return $this;
+    }
+
+    final public function all()
+    {
+        return $this->stmt->fetchAll();
+    }
+}
